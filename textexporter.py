fa381053e7363b3bc45206591b109fe27cedf1f7
Philip Mateescu <github@philipmat.net>
2017-02-06 16:21:02 -0600

    initial attempt

--- textexporter.py
+++ textexporter.py
@@ -7,41 +7,50 @@ from cStringIO import StringIO
 # - options for nulls
 # - options for separator
 
-csv.register_dialect('pgcopy', delimiter='\t')
+csv.register_dialect('pgcopy', delimiter='\t', doublequote=False, escapechar='@', quotechar='\0', quoting=csv.QUOTE_NONE)
 csv.register_dialect('pgarray', delimiter=',', doublequote=False, escapechar='\\', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+csv.register_dialect('pgquotes', delimiter=',', doublequote=True, escapechar='\\', quotechar='"', quoting=csv.QUOTE_ALL)
 
 
-def format_array(arr):
-	if not len(arr):
-		return '\\N'
-	else:
-		actuals = [prepare_value(x) for x in arr]
-		tmp_file = StringIO()
-		tmp_writer = csv.writer(tmp_file, dialect='pgarray')
-		tmp_writer.writerow(actuals)
-		tmp = tmp_file.getvalue().rstrip()
-		tmp_file.close()
-		return "{%s}" % tmp.replace("\\\"", "\\\\\"")
-
-
-def prepare_value(value):
-	if value is None or (type(value) is str and value == ''):
-		return '\\N'
-	if type(value) is list:
-		return format_array(value)
-	else:
-		if hasattr(value, 'encode'):
-			val = value.encode('utf-8')\
-				.replace('\n', '\\n')\
-				.replace('\r', '\\r')\
-				.replace('\t', '\\t')
-			return val
+class TabQuoter(object):
+	def format_array(self, arr):
+		if not len(arr):
+			return '\\N'
 		else:
-			return str(value)
+			actuals = [prepare_value(x) for x in arr]
+			tmp_file = StringIO()
+			tmp_writer = csv.writer(tmp_file, dialect='pgarray')
+			tmp_writer.writerow(actuals)
+			tmp = tmp_file.getvalue().rstrip()
+			tmp_file.close()
+			return "{%s}" % tmp.replace("\\\"", "\\\\\"")
+
+
+	def prepare_value(self, value):
+		if value is None or (type(value) is str and value == ''):
+			return '\\N'
+		if type(value) is list:
+			return format_array(value)
+		else:
+			if hasattr(value, 'encode'):
+				val = value.encode('utf-8')\
+					.replace('\n', '\\n')\
+					.replace('\r', '\\r')\
+					.replace('\t', '\\t')
+				return val
+			else:
+				return str(value)
+
+class CsvQuoter(object):
+	def format_array(self):
+		pass
+	def prepare_value(self)
+		pass
 
 
 class PgTextExporter(object):
 	def __init__(self, options, data_quality):
+		self.quoter = TabQuoter()
 		self.min_data_quality = data_quality
 		now = datetime.now()
 		self.files = {
@@ -60,6 +69,7 @@ class PgTextExporter(object):
 		config = self.files[name]
 		if not config['writer']:
 			config['file'] = StringIO()
+			# config['file'] = open(config['name'], 'wb')
 			config['writer'] = csv.writer(config['file'], dialect='pgcopy')
 
 		return config['writer']
@@ -72,8 +82,9 @@ class PgTextExporter(object):
 				if hasattr(file, 'getvalue'):
 					out = file.getvalue()
 					print out
-				if completely_done:
+				#if completely_done:
 					file.close()
+				config['file'] = None
 
 	def storeArtist(self, artist):
 		order = [
@@ -93,10 +104,9 @@ class PgTextExporter(object):
 		for c in order:
 			if hasattr(artist, c):
 				val = getattr(artist, c)
-				row.append(prepare_value(val))
+				row.append(self.quoter.prepare_value(val))
 			else:
-				row.append(prepare_value(None))
+				row.append(self.quoter.prepare_value(None))
 
-		# if row:
-		#	self._writer('artist').writerow(row)
-		print "\t".join(row)
+		if row:
+			self._writer('artist').writerow(row)
